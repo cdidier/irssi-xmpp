77ebcd83a8ac0a9b725df653dc7a6444dc9996b0
Colin Didier <cdidier@cybione.org>
2008-08-19 14:57:55 +0000

    code style changes

--- src/fe-common/fe-xmpp-status.c
+++ src/fe-common/fe-xmpp-status.c
@@ -44,7 +44,6 @@ static char *
 get_window_name(XMPP_SERVER_REC *server)
 {
 	g_return_val_if_fail(IS_XMPP_SERVER(server), NULL);
-
 	return g_strconcat("(", (server->connrec->chatnet == NULL ||
 	    *server->connrec->chatnet == '\0') ? server->jid :
 	    server->connrec->chatnet, ")", NULL);
@@ -57,14 +56,10 @@ fe_xmpp_status_get_window_name(XMPP_SERVER_REC *server)
 	char *name;
 
 	g_return_val_if_fail(IS_XMPP_SERVER(server), NULL);
-
-	name = get_window_name(server);
-	if (name == NULL)
+	if ((name = get_window_name(server)) == NULL)
 		return NULL;
-
 	window = window_find_name(name);
 	g_free(name);
-
 	return (window != NULL) ? window->name : NULL;
 }
 
@@ -75,17 +70,13 @@ fe_xmpp_status_get_window(XMPP_SERVER_REC *server)
 	char *name;
 
 	g_return_val_if_fail(IS_XMPP_SERVER(server), NULL);
-
 	name = get_window_name(server);
-	window = window_find_name(name);
-	if (window == NULL) {
+	if ((window = window_find_name(name)) == NULL) {
 		window = window_create(NULL, TRUE);
 		window_set_name(window, name);
 		window_change_server(window, server);
 	}
-
 	g_free(name);
-
 	return window;
 }
 
@@ -101,37 +92,29 @@ sig_presence_changed(XMPP_SERVER_REC *server, const char *full_jid,
 	g_return_if_fail(IS_XMPP_SERVER(server));
 	g_return_if_fail(full_jid != NULL);
 	g_return_if_fail(0 <= show && show < XMPP_PRESENCE_SHOW_LEN);	
-
 	window = fe_xmpp_status_get_window(server);
-
 	msg = fe_xmpp_presence_show[show];
-
 	user = rosters_find_user(server->roster, full_jid, NULL, NULL);
 	name = user != NULL && user->name != NULL ?
 	    format_get_text(MODULE_NAME, NULL, server, NULL,
 		XMPPTXT_FORMAT_NAME, user->name, full_jid) :
 	    format_get_text(MODULE_NAME, NULL, server, NULL,
 		XMPPTXT_FORMAT_JID, full_jid);
-
 	if (status != NULL)
 		printformat_module_window(MODULE_NAME, window, MSGLEVEL_CRAP,
 		    XMPPTXT_PRESENCE_CHANGE_REASON, name, msg, status);
 	else
 		printformat_module_window(MODULE_NAME, window, MSGLEVEL_CRAP,
 		    XMPPTXT_PRESENCE_CHANGE, name, msg);
-	
 	g_free(name);
 }
 
 static void
 sig_setup_changed(void)
 {
-	signal_remove("xmpp presence changed",
-	    (SIGNAL_FUNC)sig_presence_changed);
-
+	signal_remove("xmpp presence changed", sig_presence_changed);
 	if (settings_get_bool("xmpp_status_window"))
-		signal_add("xmpp presence changed",
-		    (SIGNAL_FUNC)sig_presence_changed);
+		signal_add("xmpp presence changed", sig_presence_changed);
 }
 
 static void
@@ -139,7 +122,6 @@ sig_server_connecting(XMPP_SERVER_REC *server)
 {
 	if (!IS_XMPP_SERVER(server))
 		return;
-
 	if (settings_get_bool("xmpp_status_window"))
 		fe_xmpp_status_get_window(server);
 }
@@ -153,15 +135,13 @@ fe_xmpp_status_init(void)
 	settings_add_bool("xmpp_lookandfeel", "xmpp_status_window", FALSE);
 
 	if (settings_get_bool("xmpp_status_window"))
-		signal_add("xmpp presence changed",
-		    (SIGNAL_FUNC)sig_presence_changed);
+		signal_add("xmpp presence changed", sig_presence_changed);
 }
 
 void
 fe_xmpp_status_deinit(void)
 {
-	signal_remove("server connecting", (SIGNAL_FUNC)sig_server_connecting);
-	signal_remove("setup changed", (SIGNAL_FUNC)sig_setup_changed);
-	signal_remove("xmpp presence changed",
-	    (SIGNAL_FUNC)sig_presence_changed);
+	signal_remove("server connecting", sig_server_connecting);
+	signal_remove("setup changed", sig_setup_changed);
+	signal_remove("xmpp presence changed", sig_presence_changed);
 }
