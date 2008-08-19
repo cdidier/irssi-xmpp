eea58e056e3bc46c83b14d36fbf1a77a61a535e6
Colin Didier <cdidier@cybione.org>
2008-08-19 14:46:26 +0000

    reintroduce channel support in the completion (prepare to the muc support)

--- src/fe-common/xmpp-completion.c
+++ src/fe-common/xmpp-completion.c
@@ -20,6 +20,7 @@
 #include <string.h>
 
 #include "module.h"
+#include "channels.h"
 #include "channels-setup.h"
 #include "misc.h"
 #include "settings.h"
@@ -27,7 +28,6 @@
 #include "window-items.h"
 
 #include "xmpp-servers.h"
-/*#include "xmpp-channels.h"*/
 #include "xmpp-commands.h"
 #include "rosters-tools.h"
 #include "tools.h"
@@ -56,16 +56,12 @@ get_resources(XMPP_SERVER_REC *server, const char *nick,
 
 	g_return_val_if_fail(IS_XMPP_SERVER(server), NULL);
 	g_return_val_if_fail(nick != NULL, NULL);
-
 	if (resource_name != NULL)
 		len = strlen(resource_name);
-
 	list = NULL;
-
 	user = rosters_find_user(server->roster, nick, NULL, NULL);
 	if (user == NULL)
 		return NULL;
-
 	for(rl = user->resources; rl != NULL; rl = rl->next) {
 		resource = rl->data;
 		if (resource_name == NULL
@@ -74,7 +70,6 @@ get_resources(XMPP_SERVER_REC *server, const char *nick,
 			    quoted_if_space(nick, resource->name) :
 			    g_strconcat(nick, "/", resource->name, NULL));
 	}
-
 	return list;
 }
 
@@ -91,54 +86,41 @@ get_nicks(XMPP_SERVER_REC *server, const char *nick, gboolean quoted,
 	
 	g_return_val_if_fail(IS_XMPP_SERVER(server), NULL);
 	g_return_val_if_fail(nick != NULL, NULL);
-
 	len = strlen(nick);
-
 	/* resources completion */
 	resource = xmpp_extract_resource(nick);
 	if (resource != NULL) {
 		jid = xmpp_strip_resource(nick);
-
 		list = get_resources(server, jid, resource, quoted);
-
 		g_free(resource);
 		g_free(jid);
-
 		return list;
 	}
-
 	list = NULL;
 	pass2 = FALSE;
-
 again:
 	/* first complete with online contacts
 	 * then complete with offline contacts */
 	for (gl = server->roster; gl != NULL; gl = gl->next) {
-
 		for (ul = ((XMPP_ROSTER_GROUP_REC *)gl->data)->users;
 		    ul != NULL ; ul = ul->next) {
 			user = (XMPP_ROSTER_USER_REC *)ul->data;
-
 			if ((!pass2 && user->resources == NULL)
 			    || (pass2 && user->resources != NULL))
 			    	continue;
-
 			if (complete_names && user->name != NULL
 			    && g_strncasecmp(user->name, nick, len) == 0)
 				list = g_list_prepend(list, quoted ?
 				    quoted_if_space(user->name, NULL) :
 				    g_strdup(user->name));
-
 			if (g_strncasecmp(user->jid, nick, len) == 0)
 				list = g_list_prepend(list, quoted ?
 				    quoted_if_space(user->jid, NULL) :
 				    g_strdup(user->jid));
 		}
 	}
-	
 	if ((pass2 = !pass2))
 		goto again;
-
 	return list;
 }
 
@@ -151,19 +133,15 @@ sig_complete_word(GList **list, WINDOW_REC *window, const char *word,
 	g_return_if_fail(list != NULL);
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
-
-	server = XMPP_SERVER(window->active_server);
-	if (server == NULL)
+	if ((server = XMPP_SERVER(window->active_server)) == NULL)
 		return;
-
 	if (g_ascii_strncasecmp(linestart,
 	    settings_get_str("cmdchars"), 1) == 0) {
 		*list = g_list_concat(*list, get_nicks(server, *word == '"' ?
 		    word+1 : word , TRUE, TRUE));
-
-	}/* else if (!IS_XMPP_CHANNEL(window->active))
+	} else if (!IS_CHANNEL(window->active))
 		*list = g_list_concat(*list, get_nicks(server, word, FALSE,
-		    TRUE));*/
+		    TRUE));
 }
 
 static void
@@ -180,33 +158,26 @@ sig_complete_command_roster_group(GList **list, WINDOW_REC *window,
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
 	g_return_if_fail(args != NULL);
-
 	server = XMPP_SERVER(window->active_server);
 	if (server == NULL)
 		return;
-
 	len = strlen(word);
 	tmp = g_strsplit(args, " ", 2);
-
 	/* complete nicks */
 	if (tmp[0] == NULL)
 		*list = g_list_concat(*list, get_nicks(server, *word == '"' ?
 		    word+1 : word , TRUE, FALSE));
-
 	/* complete groups */
 	else if (tmp[0] != NULL && tmp[1] == NULL) {
 		for (gl = server->roster; gl != NULL; gl = gl->next) {
 			group = (XMPP_ROSTER_GROUP_REC *)gl->data;
-			
 			if (group->name != NULL &&
 			    g_ascii_strncasecmp(word, group->name, len) == 0)
 				*list = g_list_append(*list,
 				    g_strdup(group->name));
 		}
 	}
-
 	g_strfreev(tmp);
-
 	if (*list != NULL)
 		signal_stop();
 }
@@ -223,32 +194,26 @@ sig_complete_command_roster_others(GList **list, WINDOW_REC *window,
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
 	g_return_if_fail(args != NULL);
-
 	server = XMPP_SERVER(window->active_server);
 	if (server == NULL)
 		return;
-
 	len = strlen(word);
 	tmp = g_strsplit(args, " ", 2);
-
 	/* complete nicks */
 	if (tmp[0] == NULL)
 		*list = g_list_concat(*list, get_nicks(server, *word == '"' ?
 		    word+1 : word , TRUE, FALSE));
-
 	g_strfreev(tmp);
-
 	if (*list != NULL)
 		signal_stop();
 }
 
-/*
 static GList *
 get_channels(XMPP_SERVER_REC *server, const char *word)
 {
 	GSList *tmp;
 	GList *list;
-	XMPP_CHANNEL_REC *channel;
+	CHANNEL_REC *channel;
 	CHANNEL_SETUP_REC *channel_setup;
 	int len;
 	
@@ -257,14 +222,15 @@ get_channels(XMPP_SERVER_REC *server, const char *word)
 	len = strlen(word);
 	list = NULL;
 	for (tmp = server->channels; tmp != NULL; tmp = tmp->next) {
-		channel = XMPP_CHANNEL(tmp->data);
+		channel = CHANNEL(tmp->data);
 		if (channel != NULL &&
 		    g_strncasecmp(channel->name, word, len) == 0)
 			list = g_list_append(list, g_strdup(channel->name));
 	}
 	for (tmp = setupchannels; tmp != NULL; tmp = tmp->next) {
 		channel_setup = tmp->data;
-		if ((IS_XMPP_CHANNEL_SETUP(channel_setup)
+		if ((PROTO_CHECK_CAST(channel_setup, CHANNEL_SETUP_REC,
+		    chat_type, XMPP_PROTOCOL_NAME)
 		    || *channel_setup->name != '#')
 		    && g_strncasecmp(channel_setup->name, word, len) == 0
 		    && glist_find_string(list, channel_setup->name) == NULL)
@@ -273,7 +239,6 @@ get_channels(XMPP_SERVER_REC *server, const char *word)
 	}
 	return list;
 }
-*/
 
 static void
 sig_complete_command_channels(GList **list, WINDOW_REC *window,
@@ -284,13 +249,10 @@ sig_complete_command_channels(GList **list, WINDOW_REC *window,
 	g_return_if_fail(list != NULL);
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
-
 	server = XMPP_SERVER(window->active_server);
 	if (server == NULL)
 		return;
-
-/*	*list = get_channels(server, word);*/
-
+	*list = get_channels(server, word);
 	if (*list != NULL)
 		signal_stop();
 }
@@ -305,17 +267,14 @@ sig_complete_command_invite(GList **list, WINDOW_REC *window,
 	g_return_if_fail(list != NULL);
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
-
 	server = XMPP_SERVER(window->active_server);
 	if (server == NULL)
 		return;
-
 	/* complete channels */
 	tmp = g_strsplit(args, " ", 2);
-/*	if (tmp[0] != NULL && tmp[1] == NULL)
-		*list = get_channels(server, word);*/
+	if (tmp[0] != NULL && tmp[1] == NULL)
+		*list = get_channels(server, word);
 	g_strfreev(tmp);
-
 	if (*list != NULL)
 		signal_stop();
 }
@@ -330,96 +289,80 @@ sig_complete_command_away(GList **list, WINDOW_REC *window,
 	g_return_if_fail(list != NULL);
 	g_return_if_fail(window != NULL);
 	g_return_if_fail(word != NULL);
-
 	server = XMPP_SERVER(window->active_server);
 	if (server == NULL)
 		return;
-
 	len = strlen(word);
-
 	if (g_strncasecmp(word,
 	    xmpp_presence_show[XMPP_PRESENCE_AWAY], len) == 0)
 		*list = g_list_append(*list,
 		    g_strdup(xmpp_presence_show[XMPP_PRESENCE_AWAY]));
-
 	if (g_strncasecmp(word,
 	    xmpp_presence_show[XMPP_PRESENCE_XA], len) == 0)
 		*list = g_list_append(*list,
 		    g_strdup(xmpp_presence_show[XMPP_PRESENCE_XA]));
-
 	if (g_strncasecmp(word,
 	    xmpp_presence_show[XMPP_PRESENCE_DND], len) == 0)
 		*list = g_list_append(*list,
 		    g_strdup(xmpp_presence_show[XMPP_PRESENCE_DND]));
-
 	if (g_strncasecmp(word,
 	    xmpp_presence_show[XMPP_PRESENCE_CHAT], len) == 0)
 		*list = g_list_append(*list,
 		    g_strdup(xmpp_presence_show[XMPP_PRESENCE_CHAT]));
-
 	if (g_strncasecmp(word,
 	    xmpp_presence_show[XMPP_PRESENCE_ONLINE], len) == 0)
 		*list = g_list_append(*list, g_strdup("online"));
-
 	signal_stop();
 }
 
 void
 xmpp_completion_init(void)
 {
-	signal_add("complete word", (SIGNAL_FUNC)sig_complete_word);
+	signal_add("complete word", sig_complete_word);
 	signal_add("complete command roster group",
-	    (SIGNAL_FUNC)sig_complete_command_roster_group);
+	    sig_complete_command_roster_group);
 	signal_add("complete command roster add",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster remove",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster name",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster accept",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster deny",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster subscribe",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_add("complete command roster unsubscribe",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
-	signal_add("complete command join",
-	    (SIGNAL_FUNC)sig_complete_command_channels);
-	signal_add("complete command part",
-	    (SIGNAL_FUNC)sig_complete_command_channels);
-	signal_add("complete command invite",
-	    (SIGNAL_FUNC)sig_complete_command_invite);
-	signal_add("complete command away",
-	    (SIGNAL_FUNC)sig_complete_command_away);
+	    sig_complete_command_roster_others);
+	signal_add("complete command join", sig_complete_command_channels);
+	signal_add("complete command part", sig_complete_command_channels);
+	signal_add("complete command invite", sig_complete_command_invite);
+	signal_add("complete command away", sig_complete_command_away);
 }
 
 void
 xmpp_completion_deinit(void)
 {
-	signal_remove("complete word", (SIGNAL_FUNC)sig_complete_word);
+	signal_remove("complete word", sig_complete_word);
 	signal_remove("complete command roster group",
-	    (SIGNAL_FUNC) sig_complete_command_roster_group);
+	     sig_complete_command_roster_group);
 	signal_remove("complete command roster add",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster remove",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster name",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster accept",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster deny",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster subscribe",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
+	    sig_complete_command_roster_others);
 	signal_remove("complete command roster unsubscribe",
-	    (SIGNAL_FUNC)sig_complete_command_roster_others);
-	signal_remove("complete command join",
-	    (SIGNAL_FUNC)sig_complete_command_channels);
-	signal_remove("complete command part",
-	    (SIGNAL_FUNC)sig_complete_command_channels);
-	signal_remove("complete command invite",
-	    (SIGNAL_FUNC)sig_complete_command_invite);
-	signal_remove("complete command away",
-	    (SIGNAL_FUNC)sig_complete_command_away);
+	    sig_complete_command_roster_others);
+	signal_remove("complete command join", sig_complete_command_channels);
+	signal_remove("complete command part", sig_complete_command_channels);
+	signal_remove("complete command invite", sig_complete_command_invite);
+	signal_remove("complete command away", sig_complete_command_away);
 }
