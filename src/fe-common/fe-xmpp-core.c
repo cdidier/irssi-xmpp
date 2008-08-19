e6594cafc25a542fd4a77245156ada0d7ba20a8a
Colin Didier <cdidier@cybione.org>
2008-08-19 21:58:04 +0000

    reenable the load of fe-text module

--- src/fe-common/fe-xmpp-core.c
+++ src/fe-common/fe-xmpp-core.c
@@ -75,12 +75,12 @@ fe_xmpp_init(void)
 	module_register("xmpp", "fe");
 
 	/* load irssi-xmpp's fe-text submodule */
-/*	if (irssi_gui == IRSSI_GUI_TEXT) {
+	if (irssi_gui == IRSSI_GUI_TEXT) {
 		char *cmd_line = g_strconcat(settings_get_str("cmdchars"),
 		    "load xmpp text", NULL);
 		signal_emit("send command", 1, cmd_line);
 		g_free(cmd_line);
-	}*/
+	}
 }
 
 void
