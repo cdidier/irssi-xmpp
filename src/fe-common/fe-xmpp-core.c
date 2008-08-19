5537b1e9abcca09886d11e4cd9103542d885e050
Colin Didier <cdidier@cybione.org>
2008-08-19 15:02:40 +0000

    add infrastructure for fe-common parts of xeps support

--- src/fe-common/fe-xmpp-core.c
+++ src/fe-common/fe-xmpp-core.c
@@ -34,6 +34,7 @@
 #include "fe-rosters.h"
 #include "fe-stanzas.h"
 #include "xmpp-completion.h"
+#include "xep/fe-xep.h"
 
 static void
 sig_server_status(SERVER_REC *server, const char *msg)
@@ -69,6 +70,7 @@ fe_xmpp_init(void)
 	fe_rosters_init();
 	fe_stanzas_init();
 	xmpp_completion_init();
+	fe_xep_init();
 
 	module_register("xmpp", "fe");
 
@@ -94,6 +96,7 @@ fe_xmpp_deinit(void)
 	fe_rosters_deinit();
 	fe_stanzas_deinit();
 	xmpp_completion_deinit();
+	fe_xep_deinit();
 
 	theme_unregister();
 }
---
+++ src/fe-common/xep/fe-xep.h
@@ -0,0 +1,11 @@
+/* $Id$ */
+
+#ifndef __FE_XEP_H
+#define __FE_XEP_H
+
+__BEGIN_DECLS
+void	fe_xep_init(void);
+void	fe_xep_deinit(void);
+__END_DECLS
+
+#endif
