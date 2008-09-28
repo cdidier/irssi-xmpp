94d18ccdad4ba8b3850540ad20f925decb550cee
Colin Didier <cdidier@cybione.org>
2008-09-28 07:35:52 +0000

    revert previous commit: help in subscribe events

--- src/fe-common/fe-rosters.c
+++ src/fe-common/fe-rosters.c
@@ -208,10 +208,10 @@ sig_subscribe(XMPP_SERVER_REC *server, const char *jid, const char *status)
 	if (settings_get_bool("xmpp_status_window"))
 		printformat_module_window(MODULE_NAME,
 		    fe_xmpp_status_get_window(server), MSGLEVEL_CRAP,
-		    XMPPTXT_SUBSCRIBE, name, status, jid);
+		    XMPPTXT_SUBSCRIBE, name, status);
 	else
 		printformat_module(MODULE_NAME, server, NULL, MSGLEVEL_CRAP,
-		    XMPPTXT_SUBSCRIBE, name, status, jid);
+		    XMPPTXT_SUBSCRIBE, name, status);
 	g_free(name);
 }
 
