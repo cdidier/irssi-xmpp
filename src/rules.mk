8d93fdc3e26982673b6d9ad00b1ce32d81b583ff
Colin Didier <cdidier@cybione.org>
2009-06-03 15:10:11 +0000

    don't hide what is going on during the compilation

--- src/rules.mk
+++ src/rules.mk
@@ -7,30 +7,27 @@ OBJS = ${SRCS:.c=.o}
 all: ${LIB}
 
 .c.o:
-	@echo ${CC} -c $<
-	@${CC} -c $< -o $@ ${CFLAGS}
+	${CC} -c $< -o $@ ${CFLAGS}
 
 ${LIB}: ${OBJS}
-	@echo ${CC} -o lib$@.so
-	@${CC} -o lib$@.so ${OBJS} ${LDFLAGS}
+	${CC} -o lib$@.so ${OBJS} ${LDFLAGS}
 
 clean:
-	@echo cleaning
-	@rm -f lib${LIB}.so ${OBJS}
+	rm -f lib${LIB}.so ${OBJS}
 
 install: all
 	@echo installing the module lib${LIB}.so to ${DESTDIR}${IRSSI_LIB}/modules
-	@install -d ${DESTDIR}${IRSSI_LIB}/modules
-	@install lib${LIB}.so ${DESTDIR}${IRSSI_LIB}/modules
+	install -d ${DESTDIR}${IRSSI_LIB}/modules
+	install lib${LIB}.so ${DESTDIR}${IRSSI_LIB}/modules
 
 uninstall:
 	@echo deinstalling the module lib${LIB}.so from ${DESTDIR}${IRSSI_LIB}/modules
-	@rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib${LIB}.so
+	rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib${LIB}.so
 
 user-install:
-	@env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} install
+	env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} install
 
 user-uninstall:
-	@env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} uninstall
+	env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} uninstall
 
 .PHONY: clean install uninstall user-install user-uninstall
