d01b2d121705c88b2d4b5ed972d112b4b553fc8c
Colin Didier <cdidier@cybione.org>
2010-05-14 16:03:30 +0000

    0.51

--- src/core/xmpp.h
+++ src/core/xmpp.h
@@ -14,6 +14,6 @@ typedef struct _MUC_REC MUC_REC;
 #define XMPP_PROTOCOL (chat_protocol_lookup(XMPP_PROTOCOL_NAME))
 
 #define IRSSI_XMPP_PACKAGE "irssi-xmpp"
-#define IRSSI_XMPP_VERSION "0.51-dev"
+#define IRSSI_XMPP_VERSION "0.51"
 
 #endif
