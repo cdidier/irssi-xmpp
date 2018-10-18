cfcedaadeec759093e79e4a9ba7f499f05a496ce
Ailin Nemui <ailin@z30a.localdomain>
2018-10-18 22:43:01 +0200

    oob uploads in muc

--- src/core/xep/oob.c
+++ src/core/xep/oob.c
@@ -25,9 +25,37 @@
 #include "xmpp-servers.h"
 #include "tools.h"
 #include "disco.h"
+#include "muc.h"
 
 #define XMLNS_OOB_X	"jabber:x:oob"
 #define XMLNS_OOB_IQ	"jabber:iq:oob"
+
+static void
+lm_message_node_delete_child(LmMessageNode *child)
+{
+	LmMessageNode *n;
+	LmMessageNode *parent = child->parent;
+	child->parent = NULL;
+	for (n = parent->children; n; ) {
+		LmMessageNode *next = n->next;
+		if (n == child) {
+			LmMessageNode *prev = n->prev;
+			if (next != NULL) {
+				next->prev = prev;
+			}
+			if (prev != NULL) {
+				prev->next = next;
+			}
+			if (n == parent->children) {
+				parent->children = next;
+			}
+			n->prev = NULL;
+			n->next = NULL;
+			lm_message_node_unref(n);
+		}
+		n = next;
+	}
+}
 
 static void
 sig_recv_x(XMPP_SERVER_REC *server, LmMessage *lmsg, const int type,
@@ -45,6 +73,12 @@ sig_recv_x(XMPP_SERVER_REC *server, LmMessage *lmsg, const int type,
 		url = child->value;
 		child = lm_message_node_get_child(node, "desc");
 		desc = child != NULL ? child->value : NULL;
+		if (lm_message_get_type(lmsg) == LM_MESSAGE_TYPE_MESSAGE) {
+			LmMessageNode *body = lm_message_node_get_child(lmsg->node, "body");
+			if (body != NULL && g_strcmp0(url, lm_message_node_get_value(body)) == 0) {
+				lm_message_node_delete_child(body);
+			}
+		}
 		url_recoded = xmpp_recode_in(url);
 		if (desc != NULL) {
 			desc_recoded = xmpp_recode_in(desc);
@@ -53,7 +87,20 @@ sig_recv_x(XMPP_SERVER_REC *server, LmMessage *lmsg, const int type,
 			g_free(desc_recoded);
 		} else
 			str = url_recoded;
-		signal_emit("message private", 4, server, str, from, from);
+		if (lm_message_get_sub_type(lmsg) == LM_MESSAGE_SUB_TYPE_GROUPCHAT) {
+			char *nick, *channel;
+
+			nick = muc_extract_nick(from);
+			channel = muc_extract_channel(from);
+
+			signal_emit("message public", 5,
+				    server, str, nick, "", channel);
+
+			g_free(channel);
+			g_free(nick);
+		} else {
+			signal_emit("message private", 4, server, str, from, from);
+		}
 		g_free(str);
 	}
 }
