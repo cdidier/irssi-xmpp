9eb8f212c976e8b5099abe01ac2f9e12279497e2
Ailin Nemui <ailin@devio.us>
2019-01-29 23:06:04 +0100

    recreate bound queries after connect

--- src/fe-common/fe-xmpp-queries.c
+++ src/fe-common/fe-xmpp-queries.c
@@ -26,6 +26,9 @@
 #include "rosters-tools.h"
 #include "fe-xmpp-status.h"
 
+int query_type;
+int chatnet_type;
+
 static void
 sig_presence_changed(XMPP_SERVER_REC *server, const char *full_jid,
     int show, const char *status)
@@ -82,11 +85,46 @@ sig_query_created(XMPP_QUERY_REC *query, int automatic)
 	    MSGLEVEL_CRAP, XMPPTXT_QUERY_AKA, user->jid, user->name);
 }
 
+static void
+sig_window_bound_query(SERVER_REC *gserver)
+{
+	XMPP_SERVER_REC *server;
+	g_return_if_fail(gserver != NULL);
+
+	if ((server = XMPP_SERVER(gserver)) == NULL)
+		return;
+
+#if defined(IRSSI_ABI_VERSION) && IRSSI_ABI_VERSION >= 20
+	GSList *tmp, *tmp2, *bounds;
+	for (tmp = windows; tmp != NULL; tmp = tmp->next) {
+		WINDOW_REC *win = tmp->data;
+		bounds = g_slist_copy(win->bound_items);
+
+		for (tmp2 = bounds; tmp2 != NULL; tmp2 = tmp2->next) {
+			WINDOW_BIND_REC *bound = tmp2->data;
+
+			if (bound->type == query_type) {
+				if (server->chat_type == chatnet_type &&
+				    g_strcmp0(server->tag, bound->servertag) == 0) {
+					xmpp_query_create(bound->servertag, bound->name, TRUE);
+				}
+			}
+		}
+
+		g_slist_free(bounds);
+	}
+#endif
+}
+
 void
 fe_xmpp_queries_init(void)
 {
+	query_type = module_get_uniq_id_str("WINDOW ITEM TYPE", "QUERY");
+	chatnet_type = XMPP_PROTOCOL;
+
 	signal_add("xmpp query raise", sig_query_raise);
 	signal_add("xmpp presence changed", sig_presence_changed);
+	signal_add("server connected", sig_window_bound_query);
 	signal_add_last("query created", sig_query_created);
 }
 
@@ -95,5 +133,6 @@ fe_xmpp_queries_deinit(void)
 {   
 	signal_remove("xmpp query raise", sig_query_raise);
 	signal_remove("xmpp presence changed", sig_presence_changed);
+	signal_remove("server connected", sig_window_bound_query);
 	signal_remove("query created", sig_query_created);
 }
